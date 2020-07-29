a3d1a15f59171fa4889667c5c56a26f74273193e
Mouad-becode <54067799+Mouad-becode@users.noreply.github.com>
2020-07-29 19:06:46 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+body,
+html{
+  position: absolute;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: white;
+}
+
+.stagger-visualizer {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 17rem;
+  height: 17rem;
+  background: #000;
+  
+}
+
+.stagger-visualizer div {
+  /*position: absolute;*/
+  width: 1rem;
+  height: 1rem;
+  border-radius: 0.1px;
+  background-color: black;
+  
+}
